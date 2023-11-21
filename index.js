54d9cf7567b20f624e1316cdb40f22ff4eacfb45
Mohit pal <mohitpal9513@gmail.com>
2023-11-21 12:23:23 +0530

    oopstree

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+
+
+function customMap(arr [], function callback(currentElement,index,arr []){
+
+
+    const result = [];
+    
+    for(let i= 0;i<arr.length;i++){
+    
+        result[i] = arr[i];
+    
+      }
+    return result;
+    
+         })
+    
+    {
+    
+        if(typeof arr === []){
+    
+    
+       for(let i= 0;i<arr.length;i++){
+    
+         console.log(arr[i]);
+    
+      }
+     
+    }
+    else{
+      console.log("error");
+    }
+    }
